a5e527444e1ab5d271e5e7cf3a77b935c9791533
desya.kristianto <desya.tech@gmail.com>
2021-12-11 21:55:00 +0700

    nomalisasi data ktp

--- src/app.service.ts
+++ src/app.service.ts
@@ -21,7 +21,7 @@ export class AppService {
       "Kecamatan"
     ]
   //for local image
-  let imagepath= 'C:/Users/desya/Desktop/Sembarang/ktp.jpeg'
+  let imagepath= 'C:/Users/desya/Desktop/Sembarang/ktp2.jpeg'
   //example for local nestjs image
   // let imagepath= './src/images/ktp2.jpeg'
   // Imports the Google Cloud client library
@@ -37,39 +37,68 @@ export class AppService {
     console.log(labels.filter(label => label.score > 0.8 && label.description=='Identity document'))
   });
 
-  // client.textDetection(imagepath).then((results)=>{
-  //   const labels = results[0].textAnnotations;
-  //   console.log(labels[0].description
-  //   );
-  // });
-  
- return client.textDetection(imagepath).then(results=>{
+  client.textDetection(imagepath).then(results=>{
     const labels = results[0].textAnnotations;
+    console.log(
     labels[0].description
     .replace("Tempat/Tgl Lahir : ","")
-    .replace("NIK","")
+    .replace("NIK\n","")
     .replace(": ","")
     .replace("Tempat/Tgl Lahir ","")
-    .replace("Nama","")
+    .replace("Nama\n","")
     .replace("Jenis kelamin","")
-    .replace("Alamat","")
+    .replace("Alamat\n","")
     .replace("PROVINSI ","")
     .replace("KABUPATEN ","")
     .replace("Kecamatan ","")
     .replace("Kecamatan :","")
-    .replace("Jenis Kelamin","")
+    .replace("Jenis kelamin\n","")
     .replace("LAKI LAKI","LAKI-LAKI")
-    // .replace("RT/RW","")
-    // .replace("Kel/Desa","")
-    // .replace("Agama","")
+    .replace("RT/RW\n","")
+    .replace("Kel/Desa\n","")
     // .replace("Status Perkawinan BELUM KAWIN","BELUM KAWIN")
     // .replace("Pekerjaan","")
     // .replace("Kewarganegaraan ","")
     // .replace("Berlaku Hingga","")
     // .replace("Gol. Darah :","")
-    // .replace("Gol Darah","")
-    // .replace("Status Perkawinan: ","")
-    // .replace("Ke varganegaraan: ","")
+    .replace("Gol Darah\n","")
+    .replace("KelDesa\n","")
+    .replace("RTRW\n","")
+    // .replace("Kewarganegaraan: ","") 
+    .replace(":","")
+    .replace(":","")
+    .replace(":","")
+    .replace(":","")
+    )
+  })
+  
+ return client.textDetection(imagepath).then(results=>{
+    const labels = results[0].textAnnotations;
+    // console.log(labels[0])
+    labels[0].description
+    .replace("Tempat/Tgl Lahir : ","")
+    .replace("NIK\n","")
+    .replace(": ","")
+    .replace("Tempat/Tgl Lahir ","")
+    .replace("Nama\n","")
+    .replace("Jenis kelamin","")
+    .replace("Alamat\n","")
+    .replace("PROVINSI ","")
+    .replace("KABUPATEN ","")
+    .replace("Kecamatan ","")
+    .replace("Kecamatan :","")
+    .replace("LAKI LAKI","LAKI-LAKI")
+    .replace("RT/RW\n","")
+    .replace("Kel/Desa\n","")
+    .replace("Jenis Kelamin","")
+    // .replace("Status Perkawinan BELUM KAWIN","BELUM KAWIN")
+    // .replace("Pekerjaan","")
+    // .replace("Kewarganegaraan ","")
+    // .replace("Berlaku Hingga","")
+    // .replace("Gol. Darah :","")
+    .replace("Gol Darah\n","")
+    .replace("KelDesa\n","")
+    .replace("RTRW\n","")
     // .replace("Kewarganegaraan: ","") 
     .replace(":","")
     .replace(":","")
@@ -78,9 +107,10 @@ export class AppService {
     // .replace(", ","")
     .split('\n').forEach((element, index) => {
       if(
-        index == 0 || index == 1 || index == 3 || index == 5 
-        || index == 6 || index == 7 || index == 8 
-        || index == 9 || index == 10 || index == 11 || index == 16 
+        index == 0 || index == 1 || index == 2 || index == 3 
+        || index == 4 || index == 5 || index == 6 
+        // || index == 7 || index == 8 || index == 9 || index == 10
+        // || index == 11 || index == 12 
         // || index == 18 || index == 20
         // || index == 21 || index == 24
         ){
@@ -91,9 +121,7 @@ export class AppService {
     array.forEach((element, index)=>{
       if(index == 5){
         if(element.length >=10){
-          console.log(element)
           let temp = array[4].concat(", ").concat(array[5].replace(" ","-"))
-          // console.log("gabungan",temp)
           array.splice(4, 0, temp);
           array.splice(6,1)
           array.splice(5,1)
@@ -113,3 +141,67 @@ export class AppService {
   });
   }
 }
+
+
+
+
+// return client.textDetection(imagepath).then(results=>{
+//   const labels = results[0].textAnnotations;
+//   labels[0].description
+//   .replace("Tempat/Tgl Lahir : ","")
+//   .replace("NIK","")
+//   .replace(": ","")
+//   .replace("Tempat/Tgl Lahir ","")
+//   .replace("Nama","")
+//   .replace("Jenis kelamin","")
+//   .replace("Alamat","")
+//   .replace("PROVINSI ","")
+//   .replace("KABUPATEN ","")
+//   .replace("Kecamatan ","")
+//   .replace("Kecamatan :","")
+//   .replace("Jenis Kelamin","")
+//   .replace("LAKI LAKI","LAKI-LAKI")
+//   // .replace("RT/RW","")
+//   // .replace("Kel/Desa","")
+//   // .replace("Agama","")
+//   // .replace("Status Perkawinan BELUM KAWIN","BELUM KAWIN")
+//   // .replace("Pekerjaan","")
+//   // .replace("Kewarganegaraan ","")
+//   // .replace("Berlaku Hingga","")
+//   // .replace("Gol. Darah :","")
+//   // .replace("Gol Darah","")
+//   // .replace("Status Perkawinan: ","")
+//   // .replace("Ke varganegaraan: ","")
+//   // .replace("Kewarganegaraan: ","") 
+//   .replace(":","")
+//   .replace(":","")
+//   .replace(":","")
+//   .replace(":","")
+//   // .replace(", ","")
+//   .split('\n').forEach((element, index) => {
+//     if(
+//       index == 0 || index == 1 || index == 3 || index == 5 
+//       || index == 6 || index == 7 || index == 8 
+//       || index == 9 || index == 10 || index == 11 || index == 16 
+//       // || index == 18 || index == 20
+//       // || index == 21 || index == 24
+//       ){
+//       array.push(element);
+//     }
+//   });
+//   // cek jika tempat tangal lahir terpisah
+//   array.forEach((element, index)=>{
+//     if(index == 5){
+//       if(element.length >=10){
+//         console.log(element)
+//         let temp = array[4].concat(", ").concat(array[5].replace(" ","-"))
+//         // console.log("gabungan",temp)
+//         array.splice(4, 0, temp);
+//         array.splice(6,1)
+//         array.splice(5,1)
+//       } else{
+//         array.splice(5,1)
+//       }
+//     }
+//   })
+
